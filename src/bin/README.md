000e9c3447ef20a40a83a6d56ecb9149fc2b5e27
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-01 14:04:12 -0500

    re

--- src/bin/README.md
+++ src/bin/README.md
@@ -9,7 +9,7 @@ timeout 2s curl -N http://192.168.4.34:8080
 # bin
 source .env
 cargo run --bin stream_blocks_print
-cargo run --bin stream_accounts_created
+cargo run --bin stream_actionview__createaccount__print
 ```
 
 ---
--- src/type_live.rs
+++ src/type_live.rs
@@ -1,12 +1,24 @@
 use near_primitives::views::{
+    ActionView,
     BlockView,
-    // ChunkView,
+    ChunkView,
+    ReceiptEnumView,
+    ReceiptView,
 };
 // ===========================================
 // Wrapper for the SSE event structure: {"block": BlockView, "shards": [...]}
 #[derive(serde::Deserialize)]
 pub struct LIVE_BLOCK_EVENT {
     pub block: BlockView,
-    pub shards: Vec<serde_json::Value>,
+    pub shards: Vec<LIVE_SHARD_DATA>,
 }
 // ===========================================
+// Shard data structure from NEAR Stream
+#[derive(serde::Deserialize, Debug)]
+pub struct LIVE_SHARD_DATA {
+    pub chunk: Option<ChunkView>,
+    pub receipts: Option<Vec<ReceiptView>>,
+    pub transactions: Option<Vec<TransactionData>>,
+}
+// ===========================================
+
